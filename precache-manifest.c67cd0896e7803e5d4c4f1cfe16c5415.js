7e5ab6068d64b00a571b7793c9bc127718edaa41
Mike Lythgoe <comp3.interactive@gmail.com>
2020-07-14 19:56:36 +0100

    Updates

---
+++ precache-manifest.c67cd0896e7803e5d4c4f1cfe16c5415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d36a367030ded86831d94dd05c6c25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ea2207415ee76c81720",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6455dd974aa7fca14fab",
+    "url": "/static/js/2.5225c21d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5225c21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea2207415ee76c81720",
+    "url": "/static/js/main.034c29a4.chunk.js"
+  },
+  {
+    "revision": "82ad76e20fea31f7bae2",
+    "url": "/static/js/runtime-main.fd19cd5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
