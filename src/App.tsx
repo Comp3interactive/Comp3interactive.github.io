cef934c2441100d1f365f4c0704a4c3ceda3f754
Mike Lythgoe <michael.1.lythgoe@co-operativebank.co.uk>
2020-07-16 11:42:05 +0100

    more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Switch>
         <Route path="/" exact component={UnderConstruction} />
         <Route path="/Playground" exact component={ComponentPlayground} />
