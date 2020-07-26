3ec4ae3b20a31cb944430995e642310a83722f21
Mike Lythgoe <michael.1.lythgoe@co-operativebank.co.uk>
2020-07-26 18:37:39 +0100

    Updated assets page

---
+++ src/Content/Assets.tsx
@@ -0,0 +1,14 @@
+export const Content = {
+  itch: {
+    header: "Graphical Assets",
+    body: [
+      "You can download a selection of free and premium assets from out Itch.io page, these vary from simple UI sets all the way to a complete 900 tile asset pack which is perfect for any rogue-like dungeon crawler!",
+    ],
+  },
+  github: {
+    header: "Code Assets",
+    body: [
+      "You can download a selection of free code snippets and utilities from our GitHub page, these include things like Android vibration controllers, A comprehensive Unity Ad Manager from one of our YouTube tutorials, custom editor utilties and more! Go check it out!",
+    ],
+  },
+};
--- src/Pages/AssetCollections.tsx
+++ src/Pages/AssetCollections.tsx
@@ -3,9 +3,16 @@ import React from "react";
 import { Header } from "../Components/Header/Header";
 import { Footer } from "../Components/Footer/Footer";
 
+import { Panel } from "../Components/Card/Panel";
+
+import * as Brands from "@fortawesome/free-brands-svg-icons";
+
+import { Content } from "../Content/Assets";
+
 import { Container, Row, Col, setConfiguration } from "react-grid-system";
 
 import { Navbar } from "../Components/Navbar/Navbar";
+import { Links } from "../Utils/ExternalLinks";
 
 export const AssetCollections = () => {
   setConfiguration({ maxScreenClass: "lg" });
@@ -16,8 +23,33 @@ export const AssetCollections = () => {
       <Header headerText={"Asset.Collections()"} />
 
       <Container>
-        <Row nogutter>
-          <Col></Col>
+        <Row>
+          <Col md={6}>
+            <Panel
+              header={Content.itch.header}
+              body={Content.itch.body}
+              buttons={[
+                {
+                  buttonText: "Download Assets Here",
+                  buttonHref: Links.itch,
+                  buttonIcon: Brands.faItchIo,
+                },
+              ]}
+            />
+          </Col>
+          <Col md={6}>
+            <Panel
+              header={Content.github.header}
+              body={Content.github.body}
+              buttons={[
+                {
+                  buttonText: "Download Code Here",
+                  buttonHref: Links.github,
+                  buttonIcon: Brands.faGithub,
+                },
+              ]}
+            />
+          </Col>
         </Row>
       </Container>
 
