ce0788012fb929b402881d1a41174aa3f593dca0
Mike Lythgoe <michael.1.lythgoe@co-operativebank.co.uk>
2021-01-26 13:54:27 +0000

    Updates

---
+++ precache-manifest.fad2103936f14b3911077891622a614e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e0e5dbe5ce819f775af6a20a3c14ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75f7b7f582ee5e226c90",
+    "url": "/static/css/main.caf9f710.chunk.css"
+  },
+  {
+    "revision": "ef2f7b65c80961162d76",
+    "url": "/static/js/2.60e7bf8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.60e7bf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f7b7f582ee5e226c90",
+    "url": "/static/js/main.9477820b.chunk.js"
+  },
+  {
+    "revision": "c912f1b526f02a2dd7e5",
+    "url": "/static/js/runtime-main.1ef2ed3d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.035275009483eba3fe81ef834fb484e5.js"
+  "/precache-manifest.fad2103936f14b3911077891622a614e.js"
 );
 
 self.addEventListener('message', (event) => {
